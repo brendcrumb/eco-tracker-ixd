5745db8e165b4f9558417e1df7a4c221b1b9f3ec
Megan Dinh <mdinh@ucsd.edu>
2020-02-13 22:33:02 -0800

    Fixed goal json reading

--- routes/addgoal.js
+++ routes/addgoal.js
@@ -4,8 +4,8 @@ exports.addGoal = function(request, response) {
   var goalCat = request.query.goalCat;
   var goalText = request.query.goalText;
   var newGoal = {
-    "goalCat" = goalCat,
-    "goalText" = goalText
+    "goalCat" : goalCat,
+    "goalText" : goalText
   }
   data.goals.push(newGoal);
   response.render('goal', data);
