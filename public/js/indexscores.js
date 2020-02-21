9b9ca58938094ff0377090aedfccf0789bdcde13
Megan Dinh <mdinh@ucsd.edu>
2020-02-20 17:08:43 -0800

    commute and waste progress workgit add . also color in circle worksgit add .

--- public/js/indexscores.js
+++ public/js/indexscores.js
@@ -17,117 +17,78 @@ function initializePage() {
     var wscores = parseFloat(wscore);
     // current food score
     var fscore = $('#fpb').text();
-    fscore = coscore.replace(/[^\d.-]/g, '');
+    fscore = fscore.replace(/[^\d.-]/g, '');
     var fscores = parseFloat(fscore);
     // get input from add page
     var commutebar = sessionStorage.getItem('commute');
     if(commutebar == null || isNaN(commutebar)){commutebar = 0;}
     else{commutebar = parseFloat(commutebar);}
-    // add new score to current score
-   // var cbarscore = parseFloat(commutebar);
-
      // calculate and update new score
      var cbarscore = Math.round(commutebar*1.0194);
      cbarscore = Math.round((cbarscore)*10)/100;    
      cbarscore = cbarscore + coscores;
-
+     // tracks score to avoid repeat addition
     var oldScore = sessionStorage.getItem('prevScore');
     oldScore = parseFloat(oldScore);
     if(isNaN(oldScore) || (oldScore-cbarscore == 0)){oldScore = 0;}
     cbarscore = cbarscore + oldScore;
+    cbarscore = Math.round((cbarscore)*10)/10;
     console.log("oldScore: " + oldScore);
      sessionStorage.setItem('prevScore', cbarscore);
      sessionStorage.removeItem('commute');
-
+     // update UI
      $('#cpb').html(cbarscore + " liters");
      $('#cpb').css("width", (cbarscore*33));
-     //update emmission score
-     var eScore = cbarscore+wscores+fscores;
+     
+      // update waste
+      var wastebar = sessionStorage.getItem('waste');
+      if(wastebar == null || isNaN(wastebar)){wastebar = 0;}
+      else{
+        wastebar = parseFloat(wastebar);
+      }
+      console.log("inWaste1: " + wastebar);
+      var wbarscore = wastebar*0.3672;
+      console.log("inWaste2: " + wastebar);
+      wbarscore = (Math.round((wbarscore)*10)/10);
+      console.log("waste: " + wbarscore);    
+      wbarscore = wbarscore + wscores;
+      var oldWaste = sessionStorage.getItem('prevScoreW');
+      oldWaste = parseFloat(oldWaste);
+      if(isNaN(oldWaste) || (oldWaste-wbarscore == 0)){oldWaste = 0;}
+      wbarscore = wbarscore + oldWaste;
+      console.log("oldScoreW: " + oldScore);
+      sessionStorage.setItem('prevScoreW', wbarscore);
+      sessionStorage.removeItem('waste');
+      $('#wpb').html(wbarscore + " liters");
+      $('#wpb').css("width", (wbarscore*33));
+
+      //update emmission score
+     var eScore = parseFloat(cbarscore)+parseFloat(wbarscore)+parseFloat(fscores);
+     eScore = Math.round((eScore)*10)/10;
      $("#score").html(eScore+"L");
      // Change color of circle
-      var yellowBound = Number(5);
-      var redBound = Number(3);
+      var yellowBound = Number(10);
+      var redBound = Number(18);
+      if(eScore >= redBound)
+        { 
+          console.log("overRed");
+          $('.circle').css("border-color", "#881313");
+        }
+      else if(eScore >= yellowBound)
+        {
+          console.log("overYellow");
+          $('.circle').css("border-color", "#E3DD55");
+        }
+      else{
+        console.log("Green");
+        $('.circle').css("border-color", "green");
+      }
+      console.log("getCommute: " + commutebar);
+      console.log("currCommute: " + coscores);
+      console.log("total: " + cbarscore);
+      console.log("waste: " + wbarscore);
+      console.log("eScore: " + eScore);
       
-      if(eScore < redBound)
-        {$('#ccolor').css("borderColor", "red");}
-      else if(eScore < yellowBound)
-        {document.getElementById("ccolor").style.borderColor = "yellow";}
-      else{$('#ccolor').css("border-outline", "green");}
-    console.log("getCommute: " + commutebar);
-    console.log("currCommute: " + coscores);
-    console.log("total: " + cbarscore);
-    console.log("eScore: " + eScore);
-      
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-		$("#testjs").text("Please wait...");
-		$(".jumbotron p").toggleClass("active");
-	});
-	$("a.thumbnail").click(projectClick);
-
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
-
-function projectClick(e) { 
-    // prevent the page from reloading      
-    e.preventDefault();
-    var containingProject = $(this).closest(".project");
-    var description = $(containingProject).find(".project-description");
-    if (description.length == 0) {
-       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
-    } 
-    else {
-       $(".project-description").fadeToggle();
-    }
-}
-/*
-     // current commute score
-      var coscore = document.getElementById("cpb").innerHTML;
-      coscore = coscore.replace(/[^\d.-]/g, '');
-      var coscores = parseFloat(coscore);
-      // current waste score
-      var wscore = document.getElementById("wpb").innerHTML;
-      wscore = wscore.replace(/[^\d.-]/g, '');
-      var wscores = parseFloat(wscore);
-      // current food score
-      var fscore = document.getElementById("fpb").innerHTML;
-      fscore = fscore.replace(/[^\d.-]/g, '');
-      var fscores = parseFloat(fscore);
-    // get input from add page
-    var commutebar = sessionStorage.getItem('commute');
-    if(commutebar == null){commutebar = 0;}
-    //var cprogress = document.getElementById("cpb").style.width;
-    //var cprogress = parseFloat(cprogress);
-    var oldScore = sessionStorage.getItem('prevScore');
-    if(isNaN(oldScore)){oldScore = 0;}
-            console.log("oldScore: " + oldScore);
-    var percent = parseFloat(commutebar) + coscores + parseFloat(oldScore - coscores);
-          sessionStorage.setItem('prevScore', percent);
-    console.log("getCommute: " + commutebar);
-    console.log("currCommute: " + coscores);
-    console.log("total: " + percent);
-    var finalpercent = Math.round(percent*1.0194);
-    // calculate new score
-      var cscore = Math.round((finalpercent)*10)/100;
-    // calculate new scores
-      document.getElementById("cpb").innerHTML = cscore +" liters";
-      document.getElementById("cpb").style.width = (finalpercent*1.1)+"%";
-      var eScore = cscore+wscores+fscores;
-      console.log(eScore);
-      document.getElementById("score").innerHTML = eScore + "L";
-    // Change color of circle
-      var redBound = Number(5);
-      var yellowBound = Number(3);
-      eScore = Number(eScore);
-      if(eScore < yellowBound)
-        {document.getElementById("ccolor").style.borderColor = "yellow";}
-      else if(eScore < redBound)
-        {document.getElementById("ccolor").style.borderColor = "red";}
-      else{document.getElementById("ccolor").style.borderColor = "green";}
-    //}
-    /*
-    else{
-      document.getElementById("cpb").style.width = (commutepr)+"%";
-      document.getElementById("score").innerHTML = coscores+wscores+fscores+"L";
-    }*/
