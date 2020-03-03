8f832c84a70598c90d2ff85892b7306a23521653
brendcrumb <bjew4b@gmail.com>
2020-03-02 17:02:26 -0800

    numeric input working, google analytics established

--- routes/commute.js
+++ routes/commute.js
@@ -2,6 +2,19 @@
  * GET commute page.
  */
 
+
 exports.view = function(req, res){
   res.render('add/commute');
 }
+
+/*
+exports.view = function(request, response){
+    projects['viewAlt'] = false;
+    response.render('add/commute');
+};
+
+exports.viewAlt = function(request, response){
+    projects['viewAlt'] = true
+    response.render('add/commute');
+};
+*/
